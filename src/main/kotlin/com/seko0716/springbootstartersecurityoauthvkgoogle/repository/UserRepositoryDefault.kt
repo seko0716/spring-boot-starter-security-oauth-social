2f0c8d701c38e02638a3cfd695ac77c24573d43a
seko0716 <sergey3440@gmail.com>
2018-04-03 12:36:50 +0400

    refactoring

--- src/main/kotlin/com/seko0716/springbootstartersecurityoauthvkgoogle/repository/UserRepositoryDefault.kt
+++ src/main/kotlin/com/seko0716/springbootstartersecurityoauthvkgoogle/repository/UserRepositoryDefault.kt
@@ -3,8 +3,9 @@ package com.seko0716.springbootstartersecurityoauthvkgoogle.repository
 import com.seko0716.springbootstartersecurityoauthvkgoogle.domains.User
 import org.bson.types.ObjectId
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
-//@Repository
+@Repository
 interface UserRepositoryDefault : CrudRepository<User, ObjectId> {
     fun findOneByLogin(login: String): User?
 }
