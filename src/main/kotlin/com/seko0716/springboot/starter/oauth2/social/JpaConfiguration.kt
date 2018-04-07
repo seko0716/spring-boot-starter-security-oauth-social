3d6771ee9c126cd367f55acd6afe12c1689c3bd4
Sergey34 <Sergey3440@gmail.com>
2018-04-07 15:14:17 +0400

    fixed build

--- src/main/kotlin/com/seko0716/springboot/starter/oauth2/social/JpaConfiguration.kt
+++ src/main/kotlin/com/seko0716/springboot/starter/oauth2/social/JpaConfiguration.kt
@@ -1,9 +1,16 @@
 package com.seko0716.springboot.starter.oauth2.social
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
+import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import javax.annotation.PostConstruct
 
 @ConditionalOnClass(name = ["org.springframework.data.jpa.repository.config.EnableJpaRepositories"])
 @EnableJpaRepositories(basePackages = ["com.seko0716.springboot.starter.oauth2.social.repository"])
+@Configuration
 class JpaConfiguration {
+    @PostConstruct
+    fun init(): Unit {
+        println("JPA")
+    }
 }
